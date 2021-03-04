854456dc86a801cb510baf2708baa26a5e065e67
Viktoria <viktoria.mihaylova92@gmail.com>
2021-03-04 21:07:35 +0100

    Review form improved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import React, { Component } from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
+import AddReview from "./components/AddReview";
+import axios from "axios";
+import { render } from "react-dom";
+import config from "./config";
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  state = {
+    restaurants: [],
+    loggedInUser: null,
+    error: null,
+    reviews: [],
+  };
+
+  handleSubmit = (data) => {
+    
+    axios.post(`${config.API_URL}/api/add-review`, 
+        data
+      )
+
+      .then((response) => {
+        this.setState(
+          {
+            reviews: [response.data, ...this.state.reviews],
+          },
+          () => {
+            this.props.history.push("/");
+          }
+        );
+      })
+      .catch((err) => {
+        console.log("adding review failed", err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+            path="/add-review"
+            render={() => {
+              return <AddReview onAdd={this.handleSubmit} />;
+            }}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -1,26 +1,65 @@
-import React, { Component } from 'react'
-
-state={
-    curTime : new Date().toLocaleString(),
-  }
+import React, { Component } from "react";
+import Rating from "react-rating";
+import { Star, StarFill, StarHalf } from "react-bootstrap-icons";
 
 class AddReview extends Component {
-    render() {
-        return (
-            <form onSubmit = {this.props.onAdd}>
-                <label> Title of your review </label>
-                <input name = "title" type = "text" placeholder="Summarize your visit"/>
-                <label> Your review </label>
-                <input name = "descriptiton" type = "text" placeholder = "Describe your experience"/> 
-                <p> Day of posting: {this.state.curTime}</p>
-                <label for="photo"> Do you have photos to share? (only in jpg or png format)</label>
-                <input type="file" name="image" autocomplete="off"></input>
-      
+  state = {
+    ratingCategory: 0,
+  };
 
-            </form>
-                
-        )
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    let title = event.target.title.value;
+    let description = event.target.description.value;
+    let date = event.target.date.value;
+    let image = event.target.image.value;
+    this.props.onAdd({
+        title,
+        description,
+        date,
+        image,
+        rating: this.state.ratingCategory
+    }) 
+  };
+
+  render() {
+    const { onAdd } = this.props;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label> Title of your review </label>
+        <input name="title" type="text" placeholder="Summarize your visit" />
+        <label> Your review </label>
+        <input
+          name="description"
+          type="text"
+          placeholder="Describe your experience"
+        />
+        <label> Review date </label>
+        <input type="date" name="date" />
+        <label for="photo">
+          {" "}
+          Do you have photos to share? (only in jpg or png format)
+        </label>
+        <input type="file" name="image" autocomplete="off"></input>
+        <label> Give a rating </label>
+        <Rating
+          name="rating"
+          emptySymbol={<Star color="yellow" />}
+          fullSymbol={<StarFill color="golden" />}
+          initialRating={this.state.ratingCategory}
+          onClick={(value) => {
+            this.setState({
+              ratingCategory: value,
+            });
+          }}
+        />
+        <span> {this.state.ratingCategory}</span>
+        <button type="submit"> Add a review </button>
+      </form>
+    );
+  }
 }
 
-export default AddReview
+export default AddReview;
